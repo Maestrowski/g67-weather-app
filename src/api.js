a82e4104c725b7e6c67a4554c12c6a1661ba586e
Njebs M <njabumacfoy.w@gmail.com>
2024-03-14 02:54:53 +0000

    Managed to get all api data printing in console with current geolocation intergrated. Need to figure out how to apply to html elements next.

--- src/api.js
+++ src/api.js
@@ -6,7 +6,8 @@ export const geoApiOptions = {
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL = "http://api.openweathermap.org/geo/1.0/";
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const FORECAST_API_URL = "https://api.openweathermap.org/data/2.5/forecast";
 export const WEATHER_API_KEY = "c92e225b5cbee18c8def29eda67e343b";
