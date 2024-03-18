9ca1e4081f29d2d95f10f708d632a860ab66744d
Maksymilian Jan Matusiak <ec22765@qmul.ac.uk>
2024-03-18 13:48:25 +0000

    Completed WeatherGraph

--- src/components/WeatherGraph/WeatherGraph.js
+++ src/components/WeatherGraph/WeatherGraph.js
@@ -3,26 +3,29 @@ import "./WeatherGraph.css";
 
 const WeatherGraph = ({ lat, lon, apiKey }) => {
   const layer = "temp_new"; // Change this to the desired weather layer
-  const zoom = 10; // Adjust the zoom level as needed
+  const zoom = 3; // Adjust the zoom level as needed
 
   // Calculate tile coordinates based on latitude and longitude
   const tileX = Math.floor((lon + 180) / 360 * Math.pow(2, zoom));
   const tileY = Math.floor((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom));
-  console.log(tileX);
-  console.log(tileY);
 
-  // Replace {layer}, {z}, {x}, {y}, and {API key} with actual values
+  // Construct the tile URL
   const tileUrl = `https://tile.openweathermap.org/map/${layer}/${zoom}/${tileX}/${tileY}.png?appid=${apiKey}`;
 
-  console.log(tileUrl)
+  // Construct the target URL dynamically using latitude and longitude
+  const targetUrl = `https://zoom.earth/maps/temperature/#view=${lat},${lon},6z/model=icon`;
 
   return (
     <div className="weather-graph-container">
-      <img src={tileUrl} className="weather-graph-img" alt="Weather Map" />
+      <a href={targetUrl} target="_blank" rel="noopener noreferrer">
+        <img src={tileUrl} className="weather-graph-img" alt="Weather Map" />
+      </a>
     </div>
   );
 };
 
 export default WeatherGraph;
+
+
 
 
